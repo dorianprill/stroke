6567349256634c0fb2eedcda8b82b811d1c31600
Dorian Prill <dorian.prill@gmail.com>
2020-03-16 17:39:07 +0100

    add quadratic beziers and line segments; add x(), y(), derivative(), curvature() and radius(); add umbrella enum BezierSegment

--- src/lib.rs
+++ src/lib.rs
@@ -24,6 +24,13 @@ where T: Add + Add<T,Output=T> + Sub + Mul + Mul<T,Output=T> + Clone {
     }
 }
 
+pub trait Coordinate {
+    type Coordinate;
+    fn x(&self) -> Self::Coordinate;
+    fn y(&self) -> Self::Coordinate;
+}
+
+
 pub trait Distance {
     type ScalarDist;
     fn distance(&self, other: Self) -> Self::ScalarDist;
@@ -34,13 +41,21 @@ pub trait Distance {
 // TODO there has to be a better architectural solution to this... if not, generate impls with macro
 #[cfg(target_pointer_width = "64")]
 type NativeFloat = f64;
+#[cfg(target_pointer_width = "64")]
+type NativeInt   = i64;
+#[cfg(target_pointer_width = "64")]
+type NativeUInt  = u64;
+
 // same thing for 32 bit
 #[cfg(target_pointer_width = "32")]
 type NativeFloat = f32;
+#[cfg(target_pointer_width = "32")]
+type NativeInt   = i32;
+#[cfg(target_pointer_width = "32")]
+type NativeUInt  = u32;
 
 
-
-impl Distance for Point2<f64> {
+impl Distance for Point2<NativeFloat> {
     type ScalarDist = NativeFloat;
     fn distance(&self, other: Self) -> NativeFloat {
         ( ((self.x - other.x) * (self.x - other.x))
@@ -48,7 +63,7 @@ impl Distance for Point2<f64> {
     }
 }
 
-impl Distance for Point2<i64> {
+impl Distance for Point2<NativeInt> {
     type ScalarDist = NativeFloat;
     fn distance(&self, other: Self) -> NativeFloat {
         ( ((self.x - other.x) * (self.x - other.x)) as NativeFloat
@@ -56,7 +71,7 @@ impl Distance for Point2<i64> {
     }
 }
 
-impl Distance for Point2<u64> {
+impl Distance for Point2<NativeUInt> {
     type ScalarDist = NativeFloat;
     fn distance(&self, other: Self) -> NativeFloat {
         ( ((self.x - other.x) * (self.x - other.x)) as NativeFloat
@@ -65,7 +80,15 @@ impl Distance for Point2<u64> {
 }
 
 
-
+impl Coordinate for Point2<NativeFloat> {
+    type Coordinate = NativeFloat;
+    fn x(&self) -> NativeFloat {
+        self.x
+    }
+    fn y(&self) -> NativeFloat {
+        self.y
+    }
+}
 
 
 impl<T> PartialEq for Point2<T> 
@@ -120,7 +143,189 @@ where
 }
 
 
-#[allow(dead_code)]
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub struct Line<P>
+{
+    start:  P,
+    end:    P,
+}
+
+impl<P> Line<P> 
+where
+P: Add + Sub + Copy
+    + Add<P, Output = P>
+    + Sub<P, Output = P>
+    + Mul<NativeFloat, Output = P>
+    + Distance<ScalarDist = NativeFloat> 
+    + Coordinate<Coordinate = NativeFloat>,
+NativeFloat: Sub<NativeFloat, Output = NativeFloat> 
+    + Mul<NativeFloat, Output = NativeFloat> {
+
+    pub fn new(start: P, end: P) -> Self {
+        Line { 
+            start, 
+            end 
+        }
+    }
+
+    pub fn eval(&self, t: NativeFloat) -> P {
+        return self.start + (self.end - self.start) * t
+    }
+
+    /// Sample the x coordinate of the segment at t (expecting t between 0 and 1).
+    pub fn x(&self, t: NativeFloat) -> NativeFloat {
+        self.start.x() + (self.end.x() - self.start.x()) * t
+    }
+
+    /// Sample the y coordinate of the segment at t (expecting t between 0 and 1).
+    #[inline]
+    pub fn y(&self, t: NativeFloat) -> NativeFloat {
+        self.start.y() + (self.end.y() - self.start.y()) * t
+    }
+
+    /// Return the derivative function.
+    /// The derivative is also a bezier curve but of degree n-1 - In the case of a line it is just a point.
+    /// Since a point is a constant, eval() does NOT need to be called separately
+    pub fn derivative(&self) -> Point2<NativeFloat>
+    where
+    P:  Sub<P, Output = P>
+        + Add<P, Output = P>
+        + Mul<NativeFloat, Output = P>,
+    NativeFloat: Sub<NativeFloat, Output = NativeFloat> 
+        + Mul<NativeFloat, Output = NativeFloat>
+        + Float
+    {
+        return Point2 {
+            x: self.end.x() - self.start.x(),
+            y: self.end.y() - self.start.y()
+        }
+    }
+}
+
+
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub struct QuadraticBezier<P>
+{
+    start:  P,
+    ctrl:   P,
+    end:    P,
+}
+
+impl<P> QuadraticBezier<P>
+where
+P: Add + Sub + Copy
+    + Add<P, Output = P>
+    + Sub<P, Output = P>
+    + Mul<NativeFloat, Output = P>
+    + Distance<ScalarDist = NativeFloat> 
+    + Coordinate<Coordinate = NativeFloat>,
+NativeFloat: Sub<NativeFloat, Output = NativeFloat> 
+    + Mul<NativeFloat, Output = NativeFloat> {
+
+    pub fn new(start: P, ctrl: P, end: P) -> Self {
+        QuadraticBezier { 
+            start, 
+            ctrl, 
+            end 
+        }
+    }
+
+    pub fn eval(&self, t: NativeFloat) -> P {
+        let t2:     NativeFloat = t * t;
+        let one_t:  NativeFloat = 1.0 as NativeFloat - t;
+        let one_t2: NativeFloat = one_t * one_t;
+
+        self.start * one_t2
+            + self.ctrl * 2.0 as NativeFloat * one_t * t
+            + self.end * t2
+    }
+
+        /// Evaluate a CubicBezier curve at t using the numerically stable De Casteljau algorithm
+    pub fn eval_casteljau(&self, t: NativeFloat) -> P 
+    {
+        // unrolled de casteljau algorithm
+        // _1ab is the first iteration from first (a) to second (b) control point and so on
+        let ctrl_1ab = self.start + (self.ctrl - self.start) * t;
+        let ctrl_1bc   = self.ctrl + (self.end - self.ctrl) * t;
+        // second iteration, final point on the curve
+        let ctrl_2ab  = ctrl_1ab + (ctrl_1bc - ctrl_1ab) * t;
+    
+        return ctrl_2ab
+    }
+
+    /// Sample the x coordinate of the curve at t (expecting t between 0 and 1).
+    pub fn x(&self, t: NativeFloat) -> NativeFloat {
+        let t2:     NativeFloat = t * t;
+        let one_t:  NativeFloat = 1 as NativeFloat - t;
+        let one_t2: NativeFloat = one_t * one_t;
+
+        self.start.x() * one_t2 + self.ctrl.x() * 2 as NativeFloat * one_t * t + self.end.x() * t2
+    }
+
+    /// Sample the y coordinate of the curve at t (expecting t between 0 and 1).
+    pub fn y(&self, t: NativeFloat) -> NativeFloat {
+        let t2:     NativeFloat = t * t;
+        let one_t:  NativeFloat = 1 as NativeFloat - t;
+        let one_t2: NativeFloat = one_t * one_t;
+
+        self.start.y() * one_t2 + self.ctrl.y() * 2 as NativeFloat* one_t * t + self.end.y() * t2
+    }
+
+    /// Return the derivative function.
+    /// The derivative is also a bezier curve but of degree n-1 - In the case of quadratic it is just a line.
+    /// Since it returns the derivative function, eval() needs to be called separately
+    pub fn derivative(&self) -> Line<P>
+    where
+    P:  Sub<P, Output = P>
+        + Add<P, Output = P>
+        + Mul<NativeFloat, Output = P>,
+    NativeFloat: Sub<NativeFloat, Output = NativeFloat> 
+        + Mul<NativeFloat, Output = NativeFloat>
+    {
+        return Line{
+            start: (self.ctrl - self.start) * 2 as NativeFloat,
+            end:   (self.end - self.ctrl)   * 2 as NativeFloat
+        }
+    }
+
+    pub fn curvature(&self, t: NativeFloat) -> NativeFloat
+    where
+    P:  Sub<P, Output = P>
+        + Add<P, Output = P>
+        + Mul<NativeFloat, Output = P>,
+    NativeFloat: Sub<NativeFloat, Output = NativeFloat> 
+        + Mul<NativeFloat, Output = NativeFloat>
+    {
+        let d = self.derivative();
+        let dd = d.derivative();
+        let dx = d.x(t);
+        let dy = d.y(t);
+        let ddx = dd.x();
+        let ddy = dd.y();
+        let numerator = dx * ddy - ddx * dy;
+        let denominator = (dx*dx + dy*dy).powf(1.5);
+        return numerator / denominator
+    }
+
+    pub fn radius(&self, t: NativeFloat) -> NativeFloat
+    where 
+    P:  Sub<P, Output = P>
+        + Add<P, Output = P>
+        + Mul<NativeFloat, Output = P>,
+    NativeFloat: Sub<NativeFloat, Output = NativeFloat> 
+        + Mul<NativeFloat, Output = NativeFloat>
+    {
+        return 1 as NativeFloat / self.curvature(t)
+    }
+}
+
+
+/// Describes a cubic bezier curve through its control points.
+/// To actually render the curve, the eval() function must be called at desired points t
+/// t is an interpolation parameter between [0,1] (0% and 100%)
+#[derive(Copy, Clone, Debug, PartialEq)]
 pub struct CubicBezier<P>
 {
     start:  P,
@@ -129,14 +334,18 @@ pub struct CubicBezier<P>
     end:    P,
 }
 
-
 #[allow(dead_code)]
 impl<P> CubicBezier<P> 
 where 
-    P: Add + Sub + Copy + Distance<ScalarDist = NativeFloat>,
+    P: Add 
+        + Sub 
+        + Copy 
+        + Distance<ScalarDist = NativeFloat> 
+        + Coordinate<Coordinate = NativeFloat>,
 {
 
-    pub fn new(start: P, ctrl1: P, ctrl2: P,  end: P) -> Self {
+    pub fn new(start: P, ctrl1: P, ctrl2: P,  end: P) -> Self 
+    {
         CubicBezier { 
             start, 
             ctrl1, 
@@ -145,9 +354,8 @@ where
         }
     }
 
-
     /// Evaluate a CubicBezier curve at t by direct evaluation of the polynomial (not numerically stable)
-    fn eval<F>(&self, t: F) -> P 
+    pub fn eval<F>(&self, t: F) -> P 
     where 
     F: Float,
     P: Add<P, Output = P>
@@ -162,7 +370,7 @@ where
     }
 
     /// Evaluate a CubicBezier curve at t using the numerically stable De Casteljau algorithm
-    fn eval_casteljau<F>(&self, t: F) -> P 
+    pub fn eval_casteljau<F>(&self, t: F) -> P 
     where 
     F: Float,
     P: Add<P, Output = P>
@@ -184,24 +392,61 @@ where
         return ctrl_3ab
     }
 
+
+    pub fn x(&self, t: NativeFloat) -> NativeFloat
+    where
+    P: Mul<NativeFloat, Output = P>,
+    NativeFloat: Sub<NativeFloat, Output = NativeFloat> 
+        + Mul<NativeFloat, Output = NativeFloat> 
+    {
+        let t2: NativeFloat     = t * t;
+        let t3: NativeFloat     = t2 * t;
+        let one_t: NativeFloat  = (1.0 as NativeFloat) - t;
+        let one_t2: NativeFloat = one_t * one_t;
+        let one_t3: NativeFloat = one_t2 * one_t;
+
+        self.start.x() * one_t3
+            + self.ctrl1.x() * 3.0 as NativeFloat * one_t2 * t
+            + self.ctrl2.x() * 3.0 as NativeFloat * one_t * t2
+            + self.end.x() * t3
+    }
+
+    pub fn y(&self, t: NativeFloat) -> NativeFloat
+    where
+    P: Mul<NativeFloat, Output = P>,
+    NativeFloat: Sub<NativeFloat, Output = NativeFloat> 
+        + Mul<NativeFloat, Output = NativeFloat> 
+    {
+        let t2: NativeFloat     = t * t;
+        let t3: NativeFloat     = t2 * t;
+        let one_t: NativeFloat  = (1.0 as NativeFloat) - t;
+        let one_t2: NativeFloat = one_t * one_t;
+        let one_t3: NativeFloat = one_t2 * one_t;
+
+        self.start.y() * one_t3
+            + self.ctrl1.y() * 3.0 as NativeFloat * one_t2 * t
+            + self.ctrl2.y() * 3.0 as NativeFloat * one_t * t2
+            + self.end.y() * t3
+    }
+
     /// Approximates the arc length of the curve by flattening it with straight line segments.
     /// This works quite well, at ~32 segments it should already provide an error < 0.5
     /// Remember arclen also works by linear approximation, not the integral, so we have to accept error!
     /// This approximation is unfeasable if desired accuracy is greater than 2 decimal places
-    fn arclen<F>(&self, nsteps: usize) -> NativeFloat
+    fn arclen(&self, nsteps: usize) -> NativeFloat
     where 
-    F: Float,
     P: Add<P, Output = P>
         + Sub<P, Output = P>
-        + Mul<F, Output = P>,
-    NativeFloat: Sub<F, Output = F> + Mul<F, Output = F> + Into<F>
+        + Mul<NativeFloat, Output = P>,
+    NativeFloat: Sub<NativeFloat, Output = NativeFloat> 
+        + Mul<NativeFloat, Output = NativeFloat>
     {
         let stepsize: NativeFloat = 1.0/(nsteps as NativeFloat);
         let mut arclen: NativeFloat = 0.0;
         for t in 1..nsteps {
-            let t = t as NativeFloat * 1.0.into()/(nsteps as NativeFloat).into();
+            let t = t as NativeFloat * 1.0/(nsteps as NativeFloat);
             let p1 = self.eval_casteljau(t);
-            let p2 = self.eval_casteljau(t+stepsize.into());
+            let p2 = self.eval_casteljau(t+stepsize);
 
             arclen = arclen + p1.distance(p2);
         
@@ -216,7 +461,8 @@ where
     P:  Sub<P, Output = P>
         + Add<P, Output = P>
         + Mul<F, Output = P>,
-    NativeFloat: Sub<F, Output = F> + Mul<F, Output = F>,
+    NativeFloat: Sub<F, Output = F> 
+        + Mul<F, Output = F>,
     {
        // unrolled de casteljau algorithm
         // _1ab is the first iteration from first (a) to second (b) control point and so on
@@ -245,7 +491,85 @@ where
         );
     }
 
+    /// Return the derivative function.
+    /// The derivative is also a bezier curve but of degree n-1 (cubic->quadratic)
+    /// ince it returns the derivative function, eval() needs to be called separately
+    pub fn derivative(&self) -> QuadraticBezier<P>
+    where
+    P:  Sub<P, Output = P>
+        + Add<P, Output = P>
+        + Mul<NativeFloat, Output = P>,
+    NativeFloat: Sub<NativeFloat, Output = NativeFloat> 
+        + Mul<NativeFloat, Output = NativeFloat>
+    {
+        return QuadraticBezier{
+            start: (self.ctrl1 - self.start) * 3 as NativeFloat,
+            ctrl:  (self.ctrl2 - self.ctrl1) * 3 as NativeFloat,
+            end:   (self.end - self.ctrl2)   * 3 as NativeFloat
+        }
+    }
+
+
+    pub fn curvature(&self, t: NativeFloat) -> NativeFloat
+    where
+    P:  Sub<P, Output = P>
+        + Add<P, Output = P>
+        + Mul<NativeFloat, Output = P>,
+    NativeFloat: Sub<NativeFloat, Output = NativeFloat> 
+        + Mul<NativeFloat, Output = NativeFloat>
+    {
+        let d = self.derivative();
+        let dd = d.derivative();
+        let dx = d.x(t);
+        let dy = d.y(t);
+        let ddx = dd.x(t);
+        let ddy = dd.y(t);
+        let numerator = dx * ddy - ddx * dy;
+        let denominator = (dx*dx + dy*dy).powf(1.5);
+        return numerator / denominator
+    }
+
+    pub fn radius(&self, t: NativeFloat) -> NativeFloat
+    where 
+    P:  Sub<P, Output = P>
+        + Add<P, Output = P>
+        + Mul<NativeFloat, Output = P>,
+    NativeFloat: Sub<NativeFloat, Output = NativeFloat> 
+        + Mul<NativeFloat, Output = NativeFloat>
+    {
+        return 1 as NativeFloat / self.curvature(t)
+    }
 }
+
+
+
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub enum BezierSegment<P> {
+    Linear(     Line<P>),
+    Quadratic(  QuadraticBezier<P>),
+    Cubic(      CubicBezier<P>),
+}
+
+impl<P> BezierSegment<P>
+where 
+P:  Sub<P, Output = P>
+    + Add<P, Output = P>
+    + Mul<NativeFloat, Output = P>
+    + Distance<ScalarDist = NativeFloat>
+    + Coordinate< Coordinate = NativeFloat>
+    + Copy,
+NativeFloat: Sub<NativeFloat, Output = NativeFloat> 
++ Mul<NativeFloat, Output = NativeFloat> {
+    pub fn eval(&self, t: NativeFloat) -> P {
+        match self {
+            BezierSegment::Linear(segment) => segment.eval(t),
+            BezierSegment::Quadratic(segment) => segment.eval(t),
+            BezierSegment::Cubic(segment) => segment.eval(t),
+        }
+    }
+}
+
 
 
 #[cfg(test)]
@@ -395,10 +719,10 @@ mod tests
                                                 ctrl1: Point2{x:-1f64,  y:c},
                                                 ctrl2: Point2{x:-c,     y:1f64},
                                                 end:   Point2{x:0f64,   y:1f64}};
-        let circumference = bezier_quadrant_1.arclen::<NativeFloat>(nsteps) +
-                                bezier_quadrant_2.arclen::<NativeFloat>(nsteps) +
-                                bezier_quadrant_3.arclen::<NativeFloat>(nsteps) +
-                                bezier_quadrant_4.arclen::<NativeFloat>(nsteps);
+        let circumference = bezier_quadrant_1.arclen(nsteps) +
+                                bezier_quadrant_2.arclen(nsteps) +
+                                bezier_quadrant_3.arclen(nsteps) +
+                                bezier_quadrant_4.arclen(nsteps);
         //dbg!(circumference);
         //dbg!(tau);
         assert!( ((tau + max_error) > circumference) && ((tau - max_error) < circumference) );
